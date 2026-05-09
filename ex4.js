39cfbf5fde30407441ff420e4016bd7aec899b1f
leocodes2030 <leonardocostaguilherme@gmail.com>
2026-05-09 15:57:20 -0300

    Alteração no laço for

--- ex4.js
+++ ex4.js
@@ -2,6 +2,6 @@ const input = require("readline-sync");
 
 let tabuada = input.questionInt(`Digite o numero que quer para sua tabuada: `);
 
-for(let i=1; i<11; i++) {
+for(let i = 1; i<=10; i++) {
     console.log(`${tabuada} * ${i} = ${tabuada * i}`);
 }
