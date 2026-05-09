0a0555cb9f8272bc0563bed941a4b45b876dc60f
leocodes2030 <leonardocostaguilherme@gmail.com>
2026-05-09 12:36:33 -0300

    Exercicio 1 concluido com sucesso!

---
+++ ex1.js
@@ -0,0 +1,7 @@
+const input = require("readline-sync");
+
+let nome = input.question("Digite seu nome: ");
+let idade = input.questionInt("Digite sua idade: ");
+let cidade = input.question("Digite sua cidade: ");
+
+console.log("Ola! Meu nome e ${nome}, tenho ${idade} anos e sou de ${cidade}.");
