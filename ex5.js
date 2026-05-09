f0424ae2153223acb2ddfdf2346da55925d798bb
leocodes2030 <leonardocostaguilherme@gmail.com>
2026-05-09 16:06:16 -0300

    Exercicio 5 concluido!

---
+++ ex5.js
@@ -0,0 +1,16 @@
+const input = require("readline-sync");
+
+let impar = 0;
+let par = 0;
+
+for(let i=0; i<10; i++) {
+    let num = input.questionInt(`Digite o ${i+1} valor: `);
+
+    if(num % 2 == 0) {
+        par++
+    } else {
+        impar++
+    }
+}
+
+console.log(`Dentro desta lista de números existem um total de ${par} número pares e ${impar} números impares`)
