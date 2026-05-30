89478f7150c80dcc83e2b7378ac865b3363f78e7
leocodes2030 <leonardocostaguilherme@gmail.com>
2026-05-30 14:46:36 -0300

    aula 03 - objetos e arrays de objetos

---
+++ aula03/ex01.js
@@ -0,0 +1,7 @@
+const pessoa = {
+    nome: "Leo",
+    idade: 15,
+    cidade: "Cascavel"
+}
+
+console.log(`Meu nome é ${pessoa.nome}, tenho ${pessoa.idade} anos, e moro na cidade de ${pessoa.cidade}`)
